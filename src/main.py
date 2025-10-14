e583644da7b164faae2cc278f0609dd0fa09acba
Julius <julius.foverskov1@gmail.com>
2025-10-14 21:39:29 +0200

    free flowing code in main works with create and insert

--- src/main.py
+++ src/main.py
@@ -1,19 +1,35 @@
-from mysql_db import MySqlDBConnection
+from mysql_db import Database
+from dataclasses import asdict
 from pathlib import Path
 import pandas as pd
 import config
 from mysql.connector.abstracts import MySQLCursorAbstract
 import mysql.connector
 
+
+
 def main():
-    print("Hello from introduction-sql!")
-    db = mysql.connector.connect(host="localhost", port=3306, user="root", password="mypassword")
-    print(f"Database is connected: {db.is_connected()}")
-    cursor = db.cursor()
-    #with db.cursor() as cursor:
-    #    read_sql_execute(config.CREATE_DB, cursor)
-    #cursor.fetchall()
-    cursor = read_sql_execute(config.CREATE_DB, cursor, db)
+    dbconfig = config.DatabaseConfig("localhost", 3306, "root", "mypassword", "db")
+    with Database(dbconfig) as db:
+        print(f"Database is connected: {db.is_connected()}")
+        cursor = db._connection.cursor()
+        print(db.connection.is_connected())
+        cursor.execute("USE db")
+        cursor.close()
+        
+        with open(config.CREATE_DB, 'r') as f:
+            sql = f.read()
+            
+            with db.cursor() as cur:
+                for statement in sql.split(';'):
+                    stmt = statement.strip()    
+                    if stmt:
+                        cur.execute(stmt)
+                db.commit()
+                print(f"Database is connected: {db.is_connected()}")
+                
+
+
 
     #cursor.execute()
     #cursor.execute(f"INSERT INTO Categories (c)")
@@ -21,30 +37,31 @@ def main():
     data = pd.read_csv(config.COMBINED_CSV)
     sql_insert_string = "INSERT INTO orders_combined (id, date_time, customer_name, customer_email, product_name, product_price) VALUES (%s, %s, %s, %s, %s, %s)"
     values = data.values.tolist()
-    #cursor.reset()
-    
-    cursor.executemany(sql_insert_string, values)
-    #cursor.fetchall()
+    with mysql.connector.connect(**asdict(dbconfig)) as db:
+        with db.cursor() as cursor:
+            cursor.execute("USE db")
+            cursor.executemany(sql_insert_string, values)
+        db.commit()
+
+    with mysql.connector.connect(host="localhost", port=3306, user="root", password="mypassword") as db:
+        with db.cursor() as cur:
+            cur.execute("USE db")
+            cur.execute("SELECT * FROM orders_combined;")
+            results = cur.fetchall()
+
+            print("\nInserted rows:")
+            for row in results:
+                print(row)
+        print(f"Database is connected: {db.is_connected()}")
 
 
-        #cursor.execute(f"INSERT INTO Categories")
-    #data.to_sql(name="Categories", if_exists="append", index=False) 
-    #row = cursor.fetchall()
-    #print(row)
-
-def read_sql_execute(file: Path, cursor: MySQLCursorAbstract, connection) -> MySQLCursorAbstract:
+def read_sql_execute(file: Path, connection):
     """
     """
     with open(file, 'r') as f:
         sql = f.read()
-        cursor.execute(sql)
-        connection.commit()
-    
-    return cursor
-
-
-
-
-
+        with connection.cursor() as cur:
+            cur.execute(sql)
+ 
 if __name__ == "__main__":
     main()
